68ee50303f8802602e18d640a6426b3a6045f574
Miccel <miccel88@gmail.com>
2018-04-29 10:27:01 +0200

    #7 Risolvere Bug nel componente Form

---
+++ client/src/app/models/candidate.ts
@@ -0,0 +1,11 @@
+export class Candidate {
+    _id                         :   string;
+    candidate_name              :   string;
+    candidate_surname           :   string;
+    candidate_age               :   string;
+    candidate_email             :   string;
+    candidate_telephone         :   string;
+    candidate_education         :   number;
+    candidate_career            :   number;
+    candidate_location          :   number;
+}
--- client/src/app/shared/candidates.service.ts
+++ client/src/app/shared/candidates.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import {Candidate}    from '../models/candidate';
+
+
+//Importo la funzione map
+import 'rxjs/add/operator/map';
+//Importo l'oggetto Observable
+
 
 @Injectable()
 export class CandidatesService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+
+  //Ottengo i contatti salvati sul DB
+  getCandidates() {
+    return this.http.get("http://localhost:3000/api/candidate")
+    .map(res => {res.json()});
+  }
+
+  addCandidate(newCandidate){
+      var headers = new Headers();
+      headers.append("Content-Type", "application/json");
+      return this.http.post("http://localhost:3000/api/candidate",newCandidate, {headers:headers})
+      .map(res =>{res.json()});
+  }
+
+  eliminateCandidate(id){
+    return this.http.delete("http://localhost:3000/api/candidate"+id)
+    .map(res =>{res.json()});
+  }
 
 }
